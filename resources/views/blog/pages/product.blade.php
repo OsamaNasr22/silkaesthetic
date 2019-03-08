5d48887b8db7d199beacc141f426e60c42e0fde4
Osama Nasr <os.ns2013@gmail.com>
2019-03-08 18:07:07 +0200

    add slider to product page

--- resources/views/blog/pages/product.blade.php
+++ resources/views/blog/pages/product.blade.php
@@ -18,7 +18,7 @@
         </div>
 
 
-        <div id="myCarousel" class="carousel slide">
+     {{--   <div id="myCarousel" class="carousel slide">
 
             <div class="carousel-inner">
                 <div class="item active">
@@ -50,6 +50,88 @@
                 <span class="glyphicon glyphicon-chevron-right" aria-hidden="true" style="color: #000;right: -12px"></span>
                 <span class="sr-only">Next</span>
             </a>
+        </div>--}}
+        <div class="demo">
+      {{--      <div class="item">
+                <div class="clearfix" style="max-width:474px;">
+                    <ul id="image-gallery" class="gallery list-unstyled cS-hidden">
+                        <li data-thumb="img/thumb/cS-1.jpg">
+                            <img src="https://via.placeholder.com/150" />
+                        </li>
+                        <li data-thumb="img/thumb/cS-2.jpg">
+                            <img src="https://via.placeholder.com/150" />
+                        </li>
+                        <li data-thumb="img/thumb/cS-3.jpg">
+                            <img src="https://via.placeholder.com/150" />
+                        </li>
+                        <li data-thumb="img/thumb/cS-4.jpg">
+                            <img src="https://via.placeholder.com/150" />
+                        </li>
+                        <li data-thumb="img/thumb/cS-5.jpg">
+                            <img src="https://via.placeholder.com/150" />
+                        </li>
+                        <li data-thumb="img/thumb/cS-6.jpg">
+                            <img src="https://via.placeholder.com/150" />
+                        </li>
+                        <li data-thumb="img/thumb/cS-7.jpg">
+                            <img src="https://via.placeholder.com/150" />
+                        </li>
+                        <li data-thumb="img/thumb/cS-8.jpg">
+                            <img src="https://via.placeholder.com/150" />
+                        </li>
+                        <li data-thumb="img/thumb/cS-9.jpg">
+                            <img src="https://via.placeholder.com/150" />
+                        </li>
+                        <li data-thumb="img/thumb/cS-10.jpg">
+                            <img src="https://via.placeholder.com/150" />
+                        </li>
+                        <li data-thumb="img/thumb/cS-11.jpg">
+                            <img src="https://via.placeholder.com/150" />
+                        </li>
+                        <li data-thumb="img/thumb/cS-12.jpg">
+                            <img src="https://via.placeholder.com/150" />
+                        </li>
+                        <li data-thumb="img/thumb/cS-13.jpg">
+                            <img src="https://via.placeholder.com/150" />
+                        </li>
+                        <li data-thumb="img/thumb/cS-14.jpg">
+                            <img src="https://via.placeholder.com/150" />
+                        </li>
+                        <li data-thumb="img/thumb/cS-15.jpg">
+                            <img src="https://via.placeholder.com/150" />
+                        </li>
+                    </ul>
+                </div>
+            </div>--}}
+            <div class="item">
+                <ul id="content-slider" class="content-slider">
+                    <li>
+                        <img src="https://via.placeholder.com/150">
+                    </li>  <li>
+                        <img src="https://via.placeholder.com/150">
+                    </li>  <li>
+                        <img src="https://via.placeholder.com/150">
+                    </li>  <li>
+                        <img src="https://via.placeholder.com/150">
+                    </li>
+                    {{--<li>--}}
+                        {{--<h3>2</h3>--}}
+                    {{--</li>--}}
+                    {{--<li>--}}
+                        {{--<h3>3</h3>--}}
+                    {{--</li>--}}
+                    {{--<li>--}}
+                        {{--<h3>4</h3>--}}
+                    {{--</li>--}}
+                    {{--<li>--}}
+                        {{--<h3>5</h3>--}}
+                    {{--</li>--}}
+                    {{--<li>--}}
+                        {{--<h3>6</h3>--}}
+                    {{--</li>--}}
+                </ul>
+            </div>
+
         </div>
 
         <div class=""></div>
@@ -61,9 +143,10 @@
 @endsection
 
 @section('style')
+    <link rel="stylesheet" href="{{asset('css/lightslider.min.css')}}">
     <style>
 
-        #myCarousel{
+      /*  #myCarousel{
             width: 33%;
             margin: -60px auto;
             top: -90px;
@@ -77,7 +160,34 @@
         .carousel-inner .item         { background: white;      }
         .carousel-inner .item img{
 
-        }
+        }*/
+      ul{
+          list-style: none outside none;
+          padding-left: 0;
+          margin: 0;
+      }
+      .demo
+      {
+          position: relative;
+          top: -176px;
+          margin: 0 auto;
+          margin-bottom: -211px;
+      }
+      .demo .item{
+          margin-bottom: 60px;
+      }
+      .content-slider li{
+          /*background-color: #ed3020;*/
+          text-align: center;
+          color: #FFF;
+      }
+      .content-slider h3 {
+          margin: 0;
+          padding: 70px 0;
+      }
+      .demo{
+          width: 800px;
+      }
         .category_cover{
             width: 100%;
             height: 482px;
@@ -95,27 +205,47 @@
 
 
 @section('js')
+    <script src="{{asset('js/lightslider.min.js')}}"></script>
 <script>
    $(function () {
-       $('#myCarousel').carousel({
-           interval: 10000
+       $("#content-slider").lightSlider({
+           loop:true,
+           keyPress:true
        });
-
-       $('.carousel .item').each(function(){
-           var next = $(this).next();
-           console.log(next);
-           if (!next.length) {
-               next = $(this).siblings(':first');
-           }
-           next.children(':first-child').clone().appendTo($(this));
-
-           if (next.next().length>0) {
-               next.next().children(':first-child').clone().appendTo($(this));
-           }
-           else {
-               $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+       $('#image-gallery').lightSlider({
+           gallery:true,
+           item:1,
+           thumbItem:9,
+           slideMargin: 0,
+           speed:500,
+           auto:true,
+           loop:true,
+           onSliderLoad: function() {
+               $('#image-gallery').removeClass('cS-hidden');
            }
        });
+       /*    $('#myCarousel').carousel({
+               interval: 10000
+           });
+
+           $('.carousel .item').each(function(){
+               var next = $(this).next();
+               console.log(next);
+               if (!next.length) {
+                   next = $(this).siblings(':first');
+               }
+               next.children(':first-child').clone().appendTo($(this));
+
+               if (next.next().length>0) {
+                   next.next().children(':first-child').clone().appendTo($(this));
+               }
+               else {
+                   $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+               }
+           });*/
+
+
+
    });
 </script>
 @endsection
